09cb373880844a0b7d68f430589dc5fe1b27bc63
Rrch <rrch.cr8@gmail.com>
2021-05-05 02:32:50 -0400

    <div *ngFor="let food of foods">   <div> {{food.name}} </div>
      </div>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Food } from './food';
+import { FoodService } from './food.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'foodmanagerangularapp';
+export class AppComponent implements OnInit {
+  public foods: Food[];
+ 
+  constructor(private foodService: FoodService) {
+   
+  }
+
+  ngOnInit() {
+    this.getAllFoods();
+  }
+
+  public getAllFoods(): void {
+    this.foodService.getAllFoods().subscribe(
+      (response: Food[]) => {
+        this.foods = response;
+        console.log(this.foods);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
